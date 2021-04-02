67859ce2db9b92bf9df4bcfb0b5771bacc03c04f
nando <muhammad.rezki.ananda@gmail.com>
2021-04-02 17:26:54 +0700

    INITIAL COMMIT

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text("Latihan Container"),
+      ),
+      body: Container(
+        color: Colors.red,
+        margin: EdgeInsets.fromLTRB(10, 15, 20, 25),
+        padding: EdgeInsets.only(bottom: 20, top: 20),
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  colors: <Color>[Colors.amber, Colors.blue])),
+        ),
+      ),
+    ));
+  }
+}
